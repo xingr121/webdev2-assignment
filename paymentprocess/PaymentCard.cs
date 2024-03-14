00545ed85e00eadc8c65d627bfc3d1051bbb5db3
xingr121 <xinghuang121@gmail.com>
2024-03-14 15:48:37 -0400

    payment

---
+++ paymentprocess/PaymentCard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_consoleApp
+{
+    internal class PaymentCard
+    {
+        private double balance;
+
+       
+        public PaymentCard(double openingBalance)
+        {
+            balance = openingBalance;
+        }
+
+       
+        public override string ToString()
+        {
+            return $"The card has a balance of {balance} euros";
+        }
+
+        // Method to decrease balance when eating lunch
+        public void EatLunch()
+        {
+            if (balance >= 10.60)
+            {
+                balance -= 10.60;
+            }
+        }
+
+        // Method to decrease balance when drinking coffee
+        public void DrinkCoffee()
+        {
+            if (balance >= 2.0)
+            {
+                balance -= 2.0;
+            }
+        }
+
+        // Method to add money to the card's balance
+        public void AddMoney(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                if (balance > 150)
+                {
+                    balance = 150;
+                }
+            }
+        }
+    }
+}
---
+++ paymentprocess/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_consoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PaymentCard card = new PaymentCard(50);
+            Console.WriteLine(card);
+            card.EatLunch();
+            Console.WriteLine(card);
+
+            card.DrinkCoffee();
+            Console.WriteLine(card);
+
+
+            card.AddMoney(49.99);
+            Console.WriteLine(card);
+
+            card.AddMoney(10000.0);
+            Console.WriteLine(card);
+
+            card.AddMoney(-10);
+            Console.WriteLine(card);
+
+            Console.ReadLine();
+        }
+    }
+}
